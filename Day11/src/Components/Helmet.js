f0fe7896f84ea313898c504c0cbce39af21ff649
PrasannaKumar R <114054393+22lecs014@users.noreply.github.com>
2023-07-21 09:21:24 +0530

    Delete Day11/src directory

--- Day11/src/Components/Helmet.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Helmet = (props) => {
-  document.title = "D06 appliances -" + props.title;
-  return <div className="w-100">{props.children}</div>;
-};
-
-export default Helmet;
